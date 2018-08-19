6347ec30bc748246c9882ae957540b5e5d938bdc
Mitchell Perilstein <mitchell.perilstein@thingworx.com>
2018-08-19 10:42:50 -0400

    started an operations tree

--- simple.py
+++ simple.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env
+#!/usr/bin/env python3
 
 '''
 Simplifying assupmtions for first experiment.
@@ -8,49 +8,63 @@ Simplifying assupmtions for first experiment.
   3. A small set of operations.
 '''
 
-# from anytree import Node, RenderTree
+from anytree import Node, RenderTree
 import math
 
-cache = {}
+fact_cache = {}
 
 def fact(n):
-    global cache
-    if n in cache:
-        ans = cache[n]
+    global fact_cache
+    if n in fact_cache:
+        ans = fact_cache[n]
     elif n < 2:
         ans = 1
-        cache[n] = ans
+        fact_cache[n] = ans
     else:
         ans = 1
         for i in range(2, n+1):
             ans *= i
-        cache[n] = ans
+        fact_cache[n] = ans
     return ans
 
 def nearest_power_less_than(n, target):
     best_m = n
     kmax = int(math.floor(math.log(n, 2)))
+    z = 0
 
     # minimize m: the distance from n
     for k in range(2, kmax + 1):
         x = math.floor(math.pow(n, 1.0/k))
-        m = target - math.pow(x, k)
+        z = math.pow(x, k)
+        m = target - z
         if abs(m) < best_m:
             best_m = m
             best_k = k
             best_x = x
+            best_z = z
 
     # x^k is m distance from n
-    return (int(best_m), int(best_x), best_k)
+    return (int(best_z), int(best_m), int(best_x), best_k)
 
+power_cache = {}
+
+# Bad, but good enough to get the tree search working, then...
+# TODO: Best algorithm here
+# https://math.stackexchange.com/questions/298044/given-an-integer-how-can-i-detect-the-nearest-integer-perfect-power-efficiently#2219230
 def nearest_power(n):
-    (m1, x1, k1) = nearest_power_less_than(n, n)
-    (m2, x2, k2) = nearest_power_less_than(n + m1 + 1, n)
+    global power_cache
+    if n in power_cache:
+        return power_cache[n]
+
+    (z1, m1, x1, k1) = nearest_power_less_than(n, n)
+    (z2, m2, x2, k2) = nearest_power_less_than(n + m1 + 1, n)
 
     if m1 < m2:
-        return (m1, x1, k1)
+        power_cache[n] = (z1, m1, x1, k1)
+        return (z1, m1, x1, k1)
     else:
-        return (m2, x2, k2)
+        power_cache[n] = (z2, m2, x2, k2)
+        return (z2, m2, x2, k2)
 
 def largest_fact_less_than(n):
     best_m = n
@@ -70,41 +84,65 @@ def largest_fact_less_than(n):
     elif n >= 2:
         best_m = 2
         best_fact = 2
-    return (best_m, best_fact)
+    return (best_fact, best_m)
+
+def gen_fact():
+    for i in range(1, 130):
+        z = largest_fact_less_than(i)
+        print("    [{}, {}],".format(i, str(z)))
+
+def gen_pow():
+    for i in range(10, 100):
+        z = nearest_power(i)
+        print("    [{}, {}],".format(i, z))
 
 class Operation:
-    def __init__(self):
-        self.foo = 1
-
-    def cost(self):
-        return len(this.toStr)
+    def __init__(self, val, n, m):
+        self.n = n
+        self.m = m
+        self.val = val
+        self.cost = len(self.toStr())
 
 class OpPow(Operation):
-    def __init__(self, x, y):
-        this.x = x
-        this.y = y
-        this.n = pow(x, y)
+    def __init__(self, n):
+        (val, m, self.x, self.y) = nearest_power(n)
+        super().__init__(val, n, m)
 
     def toStr(self):
-        return "{}^{}".format(this.x, this.y)
+        return "{}^{}".format(self.x, self.y)
 
 class OpFact(Operation):
     def __init__(self, n):
-        out=n
-        while(n>1):
-            out *= n
-            n -= 1
-        self.n = out
+        (val, m) = largest_fact_less_than(n)
+        super().__init__(val, n, m)
 
     def toStr(self):
-        return "{}!".format(self.n)
+        return "{}!".format(self.m)
+
+class OpIdentity(Operation):
+    def __init__(self, n):
+        super().__init__(n, n, 0)
+
+    def toStr(self):
+        return "{}".format(self.n)
+
+allfuncs = [
+    OpFact,
+    OpPow
+]
 
 if __name__ == "__main__":
+    gen_pow()
 
-#    for i in range(1, 130):
-#        z = largest_fact_less_than(i)
-#        print("[{}, ({}, {})],".format(i, z[0], z[1]))
+if __name__ == "__main__z":
+    n = 40
+    x = OpIdentity(n)
 
-    for i in range(10, 100):
-        z = nearest_power(i)
-        print("    [{}, ({}, {}, {})],".format(i, z[0], z[1], z[2]))
+    root = Node("root", op=x)
+
+    for op in allfuncs:
+        x = op(n)
+        Node(x.toStr(), parent=root, op=x)
+
+    for pre, fill, node in RenderTree(root):
+        print("%s%s  %s %s" % (pre, node.name, node.op.n, node.op.val))
